b7208103536909d4f5418d43b5e212ac13bab546
jaredbance <jaredbance@gmail.com>
2019-04-07 15:19:54 -0600

    implemented and tested all methods from json_examples

--- router/app.py
+++ router/app.py
@@ -5,51 +5,84 @@ import json
 import requests 
 app = Flask(__name__)
 
-players = []
-mainServerIP = -1
+serverPort = 9999
+clientPort = 8080
+players = {} # (key,value) = (client_id, client_ip)
+mainServerIP = "-1"
+#mainServerIP = "127.0.0.1"
 
 ################################################# ClIENT FUCNTIONS ###################################
 
+def getURL():
+	url = "http://" + mainServerIP + ":" + str(serverPort)
+	return url
+
+def getPlayerURL(ip):
+	url = "http://" + str(ip) + ":" + str(clientPort)
+	return url
+
 #connects clients to the game
-@app.route("/connectPlayer", methods=["GET"])
-def connectPlayer():
-	ip = request.remote_addr
-	if ip not in players:
-		players.append(ip)
-	return 'Recieved'
+@app.route("/ClientJoin", methods=["POST"])
+def ClientJoin():
+	global players
+	ip = request.remote_addr #store clients ip
+	joinMsg = request.get_json(force=True) #get message from client
+	joinResponse = requests.post(getURL(), json=joinMsg) #send message to server
+	#store client_id and ip
+	clientID = joinResponse.json()['Client_ID']
+	players.update({str(clientID): str(ip)})
+	print(players)
+	return jsonify(joinResponse.json()) #send response back to client
 
 #forwards moves from the client to the main server
-@app.route("/forwardToMainServer", methods=['POST'])
-def sendMoves():
-	moves = request.get_json()
-	print (moves)
-	requests.post(mainServerIP, data=moves)
-	return 'Recieved'
+@app.route("/PlayerMovement", methods=['POST'])
+def PlayerMovement():
+	moves = request.get_json(force=True)
+	response = requests.post(getURL(), json=moves) 
+	return response.content
+
+#tells server player is ready
+@app.route("/Ready", methods=["POST"])
+def Ready():
+	readyMsg = request.get_json(force=True)
+	readyResponse = requests.post(getURL(), json=readyMsg)
+	return readyResponse.content
+
 
 ################################################# SERVER FUCNTIONS ###################################
 
 #recieves updated main server ip from a server
-@app.route("/updateMainServer", methods=['POST'])
-def updateMainServer():
-	mainServerIP = request.get_json('ip')
-	return 'Recieved'
+@app.route("/NewPrimary", methods=['POST'])
+def NewPrimary():
+	global mainServerIP
+	ip = request.get_json('IP')
+	mainServerIP = str(ip['IP'])
+	print (mainServerIP)
+	return 'ok'
 
 #forwards game state from main server to all clients
-@app.route("/updatePlayersGameState", methods=['POST'])
-def updatePlayersGameState():
-	gameState = request.get_json()
+@app.route("/GameUpdate", methods=['POST'])
+def GameUpdate():
+	gameState = request.get_json(force=True)
 	for ip in players:
-		requests.post(ip, data=gameState)
-	return 'Recieved'
+		requests.post(getPlayerURL(players[ip]), json=gameState)
+	return 'ok'
 
 #returns the main server ip
 #	-if no main server yet, sets the requesting server to the main server and returns the same servers address
-@app.route("/join", methods=['GET'])
-def join():
+@app.route("/Join", methods=['GET'])
+def Join():
 	global mainServerIP
-	if mainServerIP != -1:
-		return jsonify({'Type': 'JoinOK', "Primary_IP": str(mainServerIP)})
+	if mainServerIP != "-1":
+		return jsonify({'Type': 'JoinOK', "Primary_IP": mainServerIP})
 	else:
-		mainServerIP = request.remote_addr
-		return jsonify({'Type': 'JoinOK', "Primary_IP": str(mainServerIP)})
+		mainServerIP = str(request.remote_addr)
+		return jsonify({'Type': 'JoinOK', "Primary_IP": mainServerIP})
 
+#server tells all players game is starting
+@app.route("/GameStart", methods=['POST'])
+def GameStart():
+	gameState = request.get_json(force=True)
+	for ip in players:
+		requests.post(getPlayerURL(players[ip]), json=gameState)
+	return 'ok'
