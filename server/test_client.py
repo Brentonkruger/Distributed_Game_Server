facf7f4bb27ad502ee48e7cbb7730b4090cac101
theFRANCHESCO <brentondkruger@gmail.com>
2019-04-13 18:48:28 -0600

    updated test server and clients

--- server/test_client.py
+++ server/test_client.py
@@ -7,18 +7,33 @@ from http import client as httpclient
 import json
 import aiohttp
 
+class Message:
+    def __init__(self, opp_num, msg_body):
+        self.recieved_backups = {}
+        self.operation_number = opp_num
+        self.msg_body = msg_body
+        self.sent_to_client = False
+
+    def recieve_backup(self, backup_ip):
+        if not backup_ip in self.recieved_backups:
+            self.recieved_backups[backup_ip] = True
+            return len(self.recieved_backups)
+        else:
+            return -1
+
+    def client_sent(self):
+        self.sent_to_client = True
+
+    def get_message_number(self):
+        return self.operation_number
+
+    def get_message_body(self):
+        return self.msg_body
 
 
 async def client():
 
-    ## requests module ##
-    # r = requests.post('http://192.168.0.10:9999/resource', json = json.dumps('{"Type": "Request","Operation": ["Up","Down","Left","Right"],"Client_ID": 2,"N_Request": 5}'))
-
-    ## native python ##
-    # a_client = httpclient.HTTPConnection('192.168.0.10', 9999)
-    # a_client.request("POST", "/gamestate", body = json.dumps('{"Type": "Request","Operation": ["U","D","L","R","N"],"Client_ID": 2,"N_Request": 5}'))
-    # data = a_client.getresponse()
-
+   
     ## aiohttp ##
     async with aiohttp.ClientSession() as session:
         async with session.get('http://192.168.0.10:9999/get') as resp:
@@ -28,10 +43,6 @@ async def client():
 
     
 
-#     reader, writer = await asyncio.open_connection(str(ipaddress.ip_address('192.168.0.10')), 9999)
-#     # reader, writer = await asyncio.open_connection('192.168.1.89', 9999)
-#     writer.write("Message from client".encode())
-#     await writer.drain()
 
 asyncio.run(client())
 # client()
--- server/test_server.py
+++ server/test_server.py
@@ -3,11 +3,6 @@ from http import server
 from http.server import SimpleHTTPRequestHandler
 
 
-# async def parse_message(reader, writer):
-#         msg = await reader.read()
-#         #parse the message (json) and call the corresponding method to deal with it.
-#         print(msg.decode())
-
 
 
 def main():
@@ -15,11 +10,5 @@ def main():
         a_server = server.HTTPServer(("192.168.0.10", 9998), handler)
         a_server.serve_forever
 
-        
-#     a_server = await asyncio.start_server(parse_message, port=9998)
 
-#     async with a_server:
-#         await a_server.serve_forever()
-
-# asyncio.run(main())
 main()
